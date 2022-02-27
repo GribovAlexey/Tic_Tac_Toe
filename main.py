c80fee29cfd59adbf76d18c720c269a1b6ed4c38
GribovAlexey <winnythheepoohh@gmail.com>
2022-02-27 17:57:37 +0300

    Now here the playground initializes, all data for the game are read here too. Added make_playground function to initialize the playground, game function now reads all nessesary data and runs the game

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from bot_logic import *
-from playground import playground
+from bot_logic import Bot
+from playground import Field
+
+
+def make_playground():
+    field_height = int(input("Enter field height: "))
+    field_width = int(input("Enter field width: "))
+    playground = Field(field_height, field_width)
+    playground.draw_field()
+    return playground
 
 
 def game():
-    field = playground
+    field = make_playground()
+    victory_condition = int(
+        input("Input required number of symbols in a row to win: "))
+
+    bot_count = int(input("Input bot count: "))
+    bots = []
+    for i in range(bot_count):
+        symbol = input(f"Input {i} bot symbol: ")
+        bots.append(Bot(symbol, victory_condition=victory_condition))
+
     game_continues = True
     while game_continues:
-        field, game_continues = first_bot.turn_step(field)
-        if not game_continues:
-            break
-        field, game_continues = second_bot.turn_step(field)
+        for bot in bots:
+            field, game_continues = bot.turn_step(field)
+            field.draw_field()
+
+            if not game_continues:
+                break
 
 
 game()
