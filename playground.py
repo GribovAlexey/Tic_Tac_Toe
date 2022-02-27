e4592145cbf33d603657b22dcb10c3d4e7014ba2
GribovAlexey <winnythheepoohh@gmail.com>
2022-02-27 17:46:35 +0300

    Splited draw_field function into two: itiate_field creates a new field, draw_field only shows playground. Added gititem and setitim functions to simplify access to cells

--- playground.py
+++ playground.py
@@ -1,23 +1,26 @@
 class Field:
-    field = []
 
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.field = []
+        self.initiate_field()
+
+    def initiate_field(self):
+        for i in range(self.height):
+            self.field.append([None for j in range(self.width)])
 
     def draw_field(self):
         formatiser = f"%{self.width}s"
-        for i in range(self.height):
-            self.field.append([])
-            for j in range(self.width):
-                self.field[i].append(j + i * self.width)
-                print(formatiser % self.field[i][j], end="")
+        for i, row in enumerate(self.field):
+            for j, elem in enumerate(row):
+                draw_elem = elem if elem is not None else i * self.width + j
+                print(formatiser % draw_elem, end="")
             print()
-        print("-"*self.width**2)
-        return self.field
+        print("-" * self.width ** 2)
 
+    def __getitem__(self, point):
+        return self.field[point[0]][point[1]]
 
-field_height = int(input("Enter field height: "))
-field_width = int(input("Enter field width: "))
-playground = Field(field_height, field_width)
-playground.draw_field()
+    def __setitem__(self, point, value):
+        self.field[point[0]][point[1]] = value
